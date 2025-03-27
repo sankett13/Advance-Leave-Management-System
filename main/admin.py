0fc10365f8288823f4ac764ae88f87fb87fe029d
Shubham Patel <sanket13052004@gmail.com>
2025-03-27 23:53:40 +0530

    changes done to model

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import FacultyDetails, StudentBasicDetails, Departments, LeaveHistory
+from .models import FacultyDetail, StudentBasicDetail, StudentContactDetail, Department, LeaveHistory
 # Register your models here.
-admin.site.register(FacultyDetails)
-admin.site.register(StudentBasicDetails)
-admin.site.register(Departments)
+admin.site.register(FacultyDetail)
+admin.site.register(StudentContactDetail)
+admin.site.register(StudentBasicDetail)
+admin.site.register(Department)
 admin.site.register(LeaveHistory)
--- main/models.py
+++ main/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
-class StudentBasicDetails(models.Model):
+class StudentBasicDetail(models.Model):
     name = models.CharField(max_length=100)
     enrollment_no = models.IntegerField()  
     sp_id = models.IntegerField()        
-    department = models.ForeignKey('Departments', on_delete=models.CASCADE)    
+    department = models.ForeignKey('Department', on_delete=models.CASCADE)    
     password = models.CharField(max_length=50)
-    FA = models.ForeignKey('FacultyDetails', on_delete=models.CASCADE)         
+    FA = models.ForeignKey('FacultyDetail', on_delete=models.CASCADE)         
 
     def __str__(self):
         return f"{self.name} - {self.enrollment_no}-{self.department}-{self.FA}"
 
-class StudentContactDetails(models.Model):
-    StudentID = models.ForeignKey('StudentBasicDetails', on_delete=models.CASCADE) 
-    PhoneNumber = models.CharField(max_length=10) 
+class StudentContactDetail(models.Model):
+    StudentID = models.ForeignKey('StudentBasicDetail', on_delete=models.CASCADE) 
+    PhoneNumber = models.IntegerField()
     Email = models.CharField(max_length=255)
     Address = models.CharField(max_length=255)
     City = models.CharField(max_length=100)
-    Pincode = models.CharField(max_length=6)
+    Pincode = models.IntegerField()
     Hostel_Block = models.CharField(max_length=1)
-    Room_No = models.CharField(max_length=3)       
-    Parent_No = models.CharField(max_length=20)    
+    Room_No = models.IntegerField()   
+    Parent_No = models.IntegerField()
     Parent_Email = models.CharField(max_length=255)
 
     def __str__(self):
@@ -28,9 +28,9 @@ class StudentContactDetails(models.Model):
 
 class LeaveHistory(models.Model):
     student_id = models.CharField(max_length=20)     
-    start_date = models.CharField(max_length=20)     
-    end_date = models.CharField(max_length=20)      
-    no_of_days = models.CharField(max_length=20)   
+    start_date = models.DateField(max_length=20)     
+    end_date = models.DateField(max_length=20)      
+    no_of_days = models.IntegerField()
     reason = models.CharField(max_length=20)       
     place_to_visit = models.CharField(max_length=20)
     status_parent = models.BooleanField()
@@ -40,17 +40,19 @@ class LeaveHistory(models.Model):
     def __str__(self):
         return str(self.student_id)
 
-class Departments(models.Model):
-    dep_name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.dep_name
-
-class FacultyDetails(models.Model):
+class FacultyDetail(models.Model):
     name = models.CharField(max_length=255)
-    department = models.ForeignKey('Departments', on_delete=models.CASCADE)    
+    department = models.ForeignKey('Department', on_delete=models.CASCADE)    
     Batch_Year = models.CharField(max_length=20)    
     is_incharge = models.BooleanField()
 
     def __str__(self):
         return self.name
+
+
+class Department(models.Model):
+    dep_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.dep_name
