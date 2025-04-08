9dc91700a6e028fc6993cc5283ed1f1f77703cb7
Shubham Patel <sanket13052004@gmail.com>
2025-04-08 12:31:43 +0530

    parent approval via mail done

--- main/urls.py
+++ main/urls.py
@@ -4,13 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    # path('student/', views.student, name='student'),
-    path('fa/', views.FA, name='FA'),
-    path('hostel/', views.hostel, name='hostel'),
+    path('hostel_incharge/', views.hostel_incharge, name='hostel_incharge'),
     path('login/', views.login, name='login'),
     path("leaveapply/", views.leaveapply, name='leaveapply'),
     path("faculty/", views.faculty_advisor, name='faculty_advisor'),
     path("approve_leave/<int:leave_id>/", views.approve_leave, name='approve_leave'),
     path("reject_leave/<int:leave_id>/", views.reject_leave, name='reject_leave'),
     path('download_leave_letter/<int:leave_id>/', views.download_leave_letter, name='download_leave_letter'),
+    path('student_login/', views.student_login, name='student_login'),
+    path('logout/', views.logout, name='logout'),
+    path('send_email/', views.send_email, name='send_email'),
+    path('parent_approve/<int:leave_id>/', views.parent_approve, name='parent_approve'),
 ]
--- main/views.py
+++ main/views.py
@@ -8,6 +8,9 @@ from rest_framework.response import Response
 from django.contrib import messages
 from datetime import date
 from docx import Document
+from django.core.mail import send_mail
+from django.conf import settings
+import json
 
 # def student(request):
 #     if request.method == 'GET':
@@ -17,18 +20,17 @@ from docx import Document
 #         sp_id=request.session.get("sp_id")
 #         print("hello",sp_id)
         
-def hostel(request):
+def hostel_incharge(request):
+    if request.session.get('fp_id') is None:
+        return redirect('login')
     if request.method == 'GET':
-        today_leave = LeaveHistory.objects.filter(start_date__lte=date.today(), end_date__gte=date.today())
+        today_leave = LeaveHistory.objects.filter(start_date__lte=date.today(), end_date__gte=date.today(),status_parent="approved", status_incharge="approved", status_fa="approved")
         today_leave_count = today_leave.count()
         context = {
             'today_leave': today_leave,
             'today_leave_count': today_leave_count
         }
         return render(request, 'Hostel.html', context)
-    
-def FA(request):
-    return render(request, 'FA.html')
 
 def login(request):
     if request.method == 'GET':
@@ -42,7 +44,7 @@ def login(request):
             hostel_incharge = get_object_or_404(FacultyDetail, fp_id=user_id)
             if hostel_incharge.password == password and hostel_incharge.department.dep_name == "Sharda Hostel Incharge":
                 request.session['fp_id'] = user_id
-                return redirect('hostel')
+                return redirect('hostel_incharge')
             else:
                 messages.error(request, "Login failed. Incorrect password.")
                 raise Http404
@@ -64,24 +66,39 @@ def login(request):
                     if student.password == password:
                         request.session['sp_id'] = user_id
                         print("here in student")
-                        c_detail=StudentContactDetail.objects.get(id=student.id)
-                        current_leave = LeaveHistory.objects.filter(student_id=student.id).last()
-                        leave_history = LeaveHistory.objects.filter(student_id=student.id, status_parent="approved", status_incharge="approved", status_fa="approved")
-                        return render(request,"Student.html",{"student":student,"info":c_detail,"leave":current_leave,"leave_history":leave_history})
+                        return redirect('student_login')
+                        
                     else:
                         messages.error(request, "Login failed. Incorrect password.")
                         return redirect('login')
                 except Http404:
                     messages.error(request, "Login failed. User not found.")
                     return redirect('login')
-        
-            
+
+
+def logout(request):
+    request.session.flush()
+    return redirect('login')
+
+def student_login(request):
+    if request.session.get('sp_id') is None:
+        return redirect('login')
+    id = request.session.get('sp_id')
+    print(id)
+    student = get_object_or_404(StudentBasicDetail, sp_id=id)
+
+    c_detail=StudentContactDetail.objects.get(StudentID=student.id)
+    current_leave = LeaveHistory.objects.filter(student_id=student.id).last()
+    leave_history = LeaveHistory.objects.filter(student_id=student.id, status_parent="approved", status_incharge="approved", status_fa="approved")
+    return render(request,"Student.html",{"student":student,"info":c_detail,"leave":current_leave,"leave_history":leave_history})
+
 
 
 def leaveapply(request):
-    if request.method == 'GET':
+    if request.session.get('sp_id') is None:
+        return redirect('login')
+    elif request.method == 'GET': #:
         return render(request, 'apply_leave.html')
-
     elif request.method == 'POST':
         sp_id = request.session.get('sp_id') #Get student id from session.
         start_date_str = request.POST.get('start_date')
@@ -114,6 +131,12 @@ def leaveapply(request):
             if leave:
                 details = StudentContactDetail.objects.get(StudentID=student)
                 print(details.Parent_Email)
+                message_content = {
+                    'subject': 'Leave Application',
+                    'body': f"Dear Parents, \n\n Your child {student.name} has applied for leave from {start_date} to {end_date}. \n\nReason: {reason}\n\nPlease approve or reject the leave application.\n\n Link: http://127.0.0.1:8000/parent_approve/{leave.id}\n\nBest regards,\n\nSharda Hostel",
+                    'to': details.Parent_Email
+                }
+                send_email(message_content)
 
                 return HttpResponse("Leave Application Successful")
             else:
@@ -128,12 +151,15 @@ def leaveapply(request):
 
 
 def faculty_advisor(request):
+    if request.session.get('fp_id') is None:
+        return redirect('login')
     id = request.session.get('fp_id')
     faculty_advisor = get_object_or_404(FacultyDetail, fp_id=id)
     if faculty_advisor.is_incharge == True:
+        today = date.today()
         leave_applications = LeaveHistory.objects.filter(status_fa='approved', status_incharge='pending')
         pending_count = LeaveHistory.objects.filter(status_incharge='pending', status_fa='approved').count()
-        approved_count = LeaveHistory.objects.filter(status_incharge='approved', status_fa='approved').count()
+        approved_count = LeaveHistory.objects.filter(status_incharge='approved', status_fa='approved',start_date__gte=today).count()
         rejected_count = LeaveHistory.objects.filter(status_incharge='rejected', status_fa='approved').count()
         context = {
             'faculty_advisor': faculty_advisor,
@@ -143,16 +169,16 @@ def faculty_advisor(request):
             'rejected_count': rejected_count,
         }
 
-        return render(request, 'FA.html', context)
+        return render(request, 'faculty_advisor.html', context)
     else:
         students = StudentBasicDetail.objects.filter(Batch_Year=faculty_advisor.Batch_Year, department=faculty_advisor.department)
 
     today = date.today()
     leave_applications = LeaveHistory.objects.filter(student_id__in=students,status_parent='approved', end_date__gte=today, status_fa='pending')
 
-    pending_count = LeaveHistory.objects.filter(student_id__in=students,status_parent='approved', end_date__gte=today, status_fa='pending').count()
-    approved_count = LeaveHistory.objects.filter(student_id__in=students, status_fa='approved').count()
-    rejected_count = LeaveHistory.objects.filter(student_id__in=students, status_fa='rejected').count()
+    pending_count = LeaveHistory.objects.filter(status_parent='approved', end_date__gte=today, status_fa='pending').count()
+    approved_count = LeaveHistory.objects.filter(status_fa='approved',start_date__gte=today, end_date__gte=today).count()
+    rejected_count = LeaveHistory.objects.filter(status_fa='rejected',start_date__gte=today, end_date__gte=today).count()
 
     context = {
         'faculty_advisor': faculty_advisor,
@@ -162,11 +188,13 @@ def faculty_advisor(request):
         'rejected_count': rejected_count,
     }
 
-    return render(request, 'FA.html', context)
+    return render(request, 'faculty_advisor.html', context)
 
 
 def approve_leave(request, leave_id):
     if request.method == 'POST':
+        if request.session.get('fp_id') is None:
+            return redirect('login')
         try:
             leave = get_object_or_404(LeaveHistory, id=leave_id)
             id = request.session.get('fp_id')
@@ -183,6 +211,8 @@ def approve_leave(request, leave_id):
 
 def reject_leave(request, leave_id):
     if request.method == 'POST':
+        if request.session.get('fp_id') is None:
+            return redirect('login')
         try:
             leave = get_object_or_404(LeaveHistory, id=leave_id)
             id = request.session.get('fp_id')
@@ -198,47 +228,87 @@ def reject_leave(request, leave_id):
     return JsonResponse({'error': 'Invalid request method', 'ok': False}, status=400)
 
 def download_leave_letter(request, leave_id):
-    leave = get_object_or_404(LeaveHistory, id=leave_id)
-    student = leave.student_id  # Assuming a ForeignKey relationship
-    print(student.id)
-    StudentContactInfo = get_object_or_404(StudentContactDetail, StudentID=student.id)
-    document = Document()
-    document.add_heading("Leave Letter", level=1)
+    if request.session.get('sp_id') is None:
+        return redirect('login')
+    else: 
+        leave = get_object_or_404(LeaveHistory, id=leave_id)
+        student = leave.student_id  # Assuming a ForeignKey relationship
+        print(student.id)
+        StudentContactInfo = get_object_or_404(StudentContactDetail, StudentID=student.id)
+        document = Document()
+        document.add_heading("Leave Letter", level=1)
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run("Respected Madam, ").bold = True
-    # paragraph.add_run(f"{student.name}").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run("Respected Madam, ").bold = True
+        # paragraph.add_run(f"{student.name}").bold = True
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run(f"I {student.name} studing in {student.department} request you to kindly accept my leave.").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run(f"I {student.name} studing in {student.department} request you to kindly accept my leave.").bold = True
 
-    paragraph.add_run(f"My leave is from {leave.start_date} to {leave.end_date}, failing to which I will accept any academic penalty/or monetary penalty.").bold = True
+        paragraph.add_run(f"My leave is from {leave.start_date} to {leave.end_date}, failing to which I will accept any academic penalty/or monetary penalty.").bold = True
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run(f"Reason for seeking leave:{leave.reason}").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run(f"Reason for seeking leave:{leave.reason}").bold = True
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run(f"Hostel Block: {StudentContactInfo.Hostel_Block}").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run(f"Hostel Block: {StudentContactInfo.Hostel_Block}").bold = True
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run(f"Room Number: {StudentContactInfo.Room_No}, Hostel Name: Sharda Girls Hostel").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run(f"Room Number: {StudentContactInfo.Room_No}, Hostel Name: Sharda Girls Hostel").bold = True
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run(f"Student Contact Number: {StudentContactInfo.PhoneNumber}").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run(f"Student Contact Number: {StudentContactInfo.PhoneNumber}").bold = True
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run(f"Parent Contact Number: {StudentContactInfo.Parent_No}").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run(f"Parent Contact Number: {StudentContactInfo.Parent_No}").bold = True
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run("Thank you for your understanding.").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run("Thank you for your understanding.").bold = True
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run("Sincerely,").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run("Sincerely,").bold = True
 
-    paragraph = document.add_paragraph()
-    paragraph.add_run(f"{student.name}").bold = True
+        paragraph = document.add_paragraph()
+        paragraph.add_run(f"{student.name}").bold = True
 
-    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-    response['Content-Disposition'] = 'attachment; filename=leave_letter.docx'
-    document.save(response)
-    return response
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+        response['Content-Disposition'] = 'attachment; filename=leave_letter.docx'
+        document.save(response)
+        return response
+
+
+
+def send_email(message_content):
+    subject = message_content['subject']
+    message = message_content['body']
+    email_to = message_content['to']
+    email_from = settings.EMAIL_HOST_USER
+    
+
+    try:
+        send_mail(subject, message, email_from, [email_to], fail_silently=False)
+        return HttpResponse("Email sent successfully")
+    except Exception as e:
+        return HttpResponse(f"Error sending email: {e}")
+
+
+def parent_approve(request,leave_id):
+    if request.method == 'GET':
+        leave = LeaveHistory.objects.filter(id=leave_id,status_parent="pending")
+        print(leave)
+        context = {
+            'leave':leave
+        }
+        return render(request, 'parent_approve.html', context)
+    elif request.method == 'POST':
+        body = json.loads(request.body)
+        print(body['action'])
+        if body['action'] == 'approve':
+            leave = get_object_or_404(LeaveHistory, id=leave_id)
+            leave.status_parent = 'approved'
+            leave.save()
+        elif body['action'] == 'reject':
+            leave = get_object_or_404(LeaveHistory, id=leave_id)
+            leave.status_parent = 'rejected'
+            leave.save()
+        return JsonResponse({'message': 'Leave rejected successfully', 'ok': True}, status=200)
